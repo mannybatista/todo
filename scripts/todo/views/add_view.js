d002f44304111ac1542274ebc7f7a5a2aca380fa
Alex Azevedo <azevedo0291@gmail.com>
2014-01-27 22:45:07 -0500

    Fixed add_view so it works

--- scripts/todo/views/add_view.js
+++ scripts/todo/views/add_view.js
@@ -7,26 +7,28 @@ define(['Base', 'hbs!../templates/add.html', '../models/add'], function(Base, tm
 			'click .btn' : 'addToList',
 		},
 		initialize : function(options) {
-			this.collectionModel = options.collectionModel;
+			this.collectionModel = options.collectionModel;	
 			this.model = new Add();
 			this.listId = this.options.listId || 0;
 			console.log('list id: ' + this.listId);
+			console.log('collection model: ' + this.collectionModel);
+			console.log('model: ' + this.model);
 		},
 		addToList : function() {
 			
 			// Use the collection model instead of the add_view model - here
-			var data = this.model.toJSON();
+			var data = this.collectionModel.toJSON();
 			var lists = data['lists'];
 			var list = lists[this.listId];
+			console.log("data" + data);
+			console.log("list" + list);
 			// After you get the list from the collectionModel, add it to the collection.
 			// Similar to this, but with the list
-
-			var collection = this.collectionModel.get('data');
-			collection.push({
-				task : this.model.get('data'),
+			list['tasks'].push({
+				task : this.model.get('task'),
 			});
-			this.collectionModel.trigger('change:data');
-
+			this.collectionModel.trigger('change:task');
+			console.log('collection model: ' + this.collectionModel);
 		},
 	});
 });
